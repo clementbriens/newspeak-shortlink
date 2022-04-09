7f2a1354be8683184d0097fe8ec035d775b99722
clementbriens <50340519+clementbriens@users.noreply.github.com>
2022-04-09 21:35:06 +0200

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # newspeak-shortlink
+
+Configure your shortlink on the `newspeak.link` domain.
+This service uses the rebrand.ly API to automatically sync the `shortlink.csv`.
+
+
+## Adding your shortlink
+
+Follow these steps if you want a shortlink added or edited.
+
+`git clone https://github.com/clementbriens/newspeak-shortlink`
+
+`git checkout -b your_branch`
+
+Edit the `shortlinks.csv` file by adding your `slashtag` and `url`.
+
+`git add shortlinks.csv`
+
+`git commit -m "your commit message"`
+
+`git push`
+
+Open a pull request. Upon review, your branch will be merged and the shortlink system will be updated with the latest .csv file.
+
+## Installation
+
+Follow these steps if you want to run this service.
+
+`git clone https://github.com/clementbriens/newspeak-shortlink`
+
+`virtualenv env -p python3 && source env/bin/activate`
+
+`pip install -r requirements.txt`
+
+`mv config.ini.sample config.ini`
+
+## Configuration
+
+Add your Rebrandly API key and domain to `config.ini`.
---
+++ sync.py
@@ -0,0 +1,107 @@
+import requests
+import pandas as pd
+import json
+import configparser
+
+
+class LinkSync():
+
+    def __init__(self):
+        print('[*] LinkSync started')
+        self.config = configparser.ConfigParser()
+        self.config.read('config.ini')
+        self.url = 'https://api.rebrandly.com/v1'
+        self.api_key = self.config['config']['api_key']
+        self.domain = self.config['config']['domain']
+        self.headers = {
+          "Content-type": "application/json",
+          "apikey": self.api_key
+        }
+
+    def list_all_links(self):
+        all_links = dict()
+        paginate = True
+        id = ""
+        #while paginate:
+        r = requests.get(self.url + "/links?limit=25&last={}".format(id),
+        headers=self.headers)
+        data = r.json()
+        for d in data:
+            if d['slashtag'] not in all_links.keys():
+                all_links[d['slashtag']] = {'url' : d['destination'], 'date_added' : d['createdAt'], 'id' : d['id']}
+        return all_links
+
+    def add_link(self, slashtag, url):
+        if not url.startswith('https://'):
+            url = 'https://' + url
+        payload = {
+        'title' : url,
+        'destination' : url,
+        'domain' : {'fullName' : self.domain},
+        'slashtag' : slashtag
+        }
+        r = requests.post(self.url + '/links/', data = json.dumps(payload), headers = self.headers)
+        if r.status_code == 200:
+            print('[*] Added /{} with {}'.format(slashtag, url))
+        else:
+            print('[!] {} Could not add link.'.format(r.status_code))
+
+    def update_link(self, slashtag, url, id):
+        if not url.startswith('https://'):
+            url = 'https://' + url
+        payload = {
+        'id' : id,
+        'title' : url,
+        'destination' : url,
+        'shortUrl' : self.domain + '/'+ slashtag
+        }
+        r = requests.post(self.url + '/links/' + id, data = json.dumps(payload), headers = self.headers)
+        if r.status_code == 200:
+            print('[*] Updated /{} with {}'.format(slashtag, url))
+        else:
+            print('[!] {} Could not update link.'.format(r.status_code))
+
+    def delete_link(self, id, slashtag):
+        r = requests.delete(self.url + '/links/' + id, headers = self.headers)
+        if r.status_code == 200:
+            print('[*] Deleted {}'.format(slashtag))
+        else:
+            print('[!] {} Could not delete link.'.format(r.status_code))
+
+    def export_csv(self, links):
+        all_data = list()
+        for slashtag in links.keys():
+            data = dict()
+            data['slashtag'] = slashtag
+            for key in links[slashtag].keys():
+                if key != 'id':
+                    data[key] = links[slashtag][key]
+            all_data.append(data)
+        df = pd.DataFrame(all_data)
+        df.to_csv('shortlinks.csv')
+
+
+    def sync(self):
+        links = self.list_all_links()
+        df = pd.read_csv('shortlinks.csv')
+        for index, row in df.iterrows():
+            if not row['url'].startswith('https://'):
+                row['url'] = 'https://' + row['url']
+            if row['slashtag'] in links.keys() and row['url'] != links[row['slashtag']]['url']:
+                print('[*] Updating', row['slashtag'], 'with', row['url'])
+                self.update_link(row['slashtag'], row['url'], links[row['slashtag']]['id'])
+            if row['slashtag'] not in links.keys():
+                print('[*] Adding', row['slashtag'], 'with', row['url'])
+                self.add_link(row['slashtag'], row['url'])
+        for slashtag in links.keys():
+            if slashtag not in df['slashtag'].unique():
+                print('[*] Deleting {}'.format(slashtag))
+                self.delete_link(links[slashtag]['id'], slashtag)
+        print('[*] Done.')
+        links = self.list_all_links()
+        self.export_csv(links)
+
+
+if __name__ == '__main__':
+    ls = LinkSync()
+    ls.sync()
